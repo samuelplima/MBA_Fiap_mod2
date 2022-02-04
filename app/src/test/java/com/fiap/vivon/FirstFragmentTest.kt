2f723306d5e1338a33752115ed04df65ec7407cd
IuriMagalhaes <iurimagalhaes96@gmail.com>
2022-02-04 18:16:56 -0300

    Adicional testes

--- app/src/test/java/com/fiap/vivon/FirstFragmentTest.kt
+++ app/src/test/java/com/fiap/vivon/FirstFragmentTest.kt
@@ -10,13 +10,21 @@ class FirstFragmentTest {
 
     //teste função checkCpf
     @Test
-    fun checkCpfTest() {
+    fun checkCpfTestOkay() {
         assertEquals(true, firstFragment.checkCpf("39792784845"))
+    }
+    @Test
+    fun checkCpfTestNotOkay() {
+        assertEquals(false, firstFragment.checkCpf("43141678819"))
     }
 
     //teste função checkCnpj
     @Test
-    fun checkCnpjTest() {
+    fun checkCnpjTestOkay() {
+        assertEquals(true, firstFragment.checkCnpj("00811569000137"))
+    }
+    @Test
+    fun checkCnpjTestNotOkay() {
         assertEquals(false, firstFragment.checkCnpj("455555555555651223"))
     }
 }
