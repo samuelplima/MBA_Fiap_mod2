3ee62b70856d6376e1010e2667736a6e4bbd0ad1
Samuel Lima <samuel.lima@voffice.com.br>
2022-03-10 02:19:09 -0300

    adicionado campo telefone na tela dashboard e consulta no banco

--- app/src/main/java/com/fiap/vivo/util/BringData.kt
+++ app/src/main/java/com/fiap/vivo/util/BringData.kt
@@ -5,11 +5,10 @@ import com.fiap.vivo.viewmodel.UserViewModel
 
 class BringData {
 
-    private val identificacaoPersistencia = IdentificacaoPersistencia()
-
     fun bringData(cnpjCpf: String, binding: DashboardScreenBinding, userViewModel: UserViewModel) {
         binding.dashboardPageName.text = "Nome: " + userViewModel.findName(cnpjCpf)
         binding.dashboardPageCpfCnpj.text = "Cpf/Cnpj: " + userViewModel.findUser(cnpjCpf)
+        binding.dashboardPageTelefone.text = "Telefone: " + userViewModel.findTelefone(cnpjCpf)
         binding.dashboardPageEmail.text = "Email: " + userViewModel.findEmailWithCpfCnpj(cnpjCpf)
         binding.dashboardPagePlan.text = "Plano: " + userViewModel.findPlanos(cnpjCpf)
         binding.dashboardPageSituation.text = "Situação: " + userViewModel.findSituacao(cnpjCpf).uppercase()
